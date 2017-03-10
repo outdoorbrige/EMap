310755127d96fb84292d1b6ad7b6fa2537cdce60
gitblit <gitblit.gitblit.com>
2017-03-10 16:36:32 +0800

    添加应用安装和启动时，创建桌面快捷方式功能

--- app/src/main/java/com/gh/emap/MainActivity.java
+++ app/src/main/java/com/gh/emap/MainActivity.java
@@ -1,5 +1,8 @@
 package com.gh.emap;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -30,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getDisplayMetrics().ydpi,
                         (int)(Math.round(getResources().getDisplayMetrics().widthPixels / getResources().getDisplayMetrics().density)),
                         (int)(Math.round(getResources().getDisplayMetrics().heightPixels / getResources().getDisplayMetrics().density))));
+
+        addShortcut(getApplationName()); // 添加桌面快捷方式
     }
 
     public MainManager getMainManager() {
@@ -89,15 +94,15 @@ public class MainActivity extends AppCompatActivity {
 //            View view = findViewById(R.id.ground_render_point_type);
 //            if(!isTouchedView(view, motionEvent)) { // 隐藏BottomGroundRenderPointLayout布局
 //                getMainManager().getLayoutManager().getGroundRenderPointTypeLayout().hide();
-//                getMainManager().getLayoutManager().getMenuLayout().toastShowShort();
-//                getMainManager().getLayoutManager().getOperationLayout().toastShowShort();
+//                getMainManager().getLayoutManager().getMenuLayout().show();
+//                getMainManager().getLayoutManager().getOperationLayout().show();
 //            }
 //
 //            view = findViewById(R.id.bottom_ground_render_line);
 //            if(!isTouchedView(view, motionEvent)) { // 隐藏BottomGroundRenderLineLayout布局
 //                getMainManager().getLayoutManager().getGroundRenderLineTypeLayout().hide();
-//                getMainManager().getLayoutManager().getMenuLayout().toastShowShort();
-//                getMainManager().getLayoutManager().getOperationLayout().toastShowShort();
+//                getMainManager().getLayoutManager().getMenuLayout().show();
+//                getMainManager().getLayoutManager().getOperationLayout().show();
 //            }
 //
 //            return super.dispatchTouchEvent(motionEvent);
@@ -139,6 +144,93 @@ public class MainActivity extends AppCompatActivity {
     // 获取应用程序名称
     public String getApplationName() {
         return "EMap";
+    }
+
+    // 添加快捷方式
+    private void addShortcut(String name) {
+        // 如果存在，就取消添加
+        if(hasInstallShortcut(name)) {
+            return;
+        }
+
+        final String ACTION_ADD_SHORTCUT = "com.android.launcher.action.INSTALL_SHORTCUT";
+
+        Intent addShortcutIntent = new Intent(ACTION_ADD_SHORTCUT);
+
+        // 不允许重复创建
+        addShortcutIntent.putExtra("duplicate", false);// 经测试不是根据快捷方式的名字判断重复的
+        // 应该是根据快链的Intent来判断是否重复的,即Intent.EXTRA_SHORTCUT_INTENT字段的value
+        // 但是名称不同时，虽然有的手机系统会显示Toast提示重复，仍然会建立快链
+        // 屏幕上没有空间时会提示
+        // 注意：重复创建的行为MIUI和三星手机上不太一样，小米上似乎不能重复创建快捷方式
+
+        // 名字
+        addShortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+
+        // 图标
+        addShortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                Intent.ShortcutIconResource.fromContext(MainActivity.this,
+                        R.mipmap.ic_launcher));
+
+        // 设置关联程序
+        Intent launcherIntent = new Intent(Intent.ACTION_MAIN);
+        launcherIntent.setClass(MainActivity.this, MainActivity.class);
+        launcherIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        addShortcutIntent
+                .putExtra(Intent.EXTRA_SHORTCUT_INTENT, launcherIntent);
+
+        // 发送广播
+        sendBroadcast(addShortcutIntent);
+    }
+
+    // 移除快捷方式
+    // 在小米系统上不管用，在三星上可以移除
+    private void removeShortcut(String name) {
+        final String ACTION_REMOVE_SHORTCUT = "com.android.launcher.action.UNINSTALL_SHORTCUT";
+
+        // remove shortcut的方法在小米系统上不管用，在三星上可以移除
+        Intent intent = new Intent(ACTION_REMOVE_SHORTCUT);
+
+        // 名字
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+
+        // 设置关联程序
+        Intent launcherIntent = new Intent(MainActivity.this,
+                MainActivity.class).setAction(Intent.ACTION_MAIN);
+
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launcherIntent);
+
+        // 发送广播
+        sendBroadcast(intent);
+    }
+
+    // 查询快捷方式
+    // 查询快捷方式是否存在的方法是从网上其他资料那里查来的，但是测试查询的时候失败了，两个手机(小米、三星)都查不到。
+    // 先留着代码以后看看是什么原因吧
+    private boolean hasInstallShortcut(String name) {
+
+        boolean hasInstall = false;
+
+        final String AUTHORITY = "com.android.launcher2.settings";
+        Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+                + "/favorites?notify=true");
+
+        // 这里总是failed to find provider info
+        // com.android.launcher2.settings和com.android.launcher.settings都不行
+        Cursor cursor = this.getContentResolver().query(CONTENT_URI,
+                new String[] { "title", "iconResource" }, "title=?",
+                new String[] { name }, null);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            hasInstall = true;
+        }
+
+        if(cursor != null) {
+            cursor.close();
+        }
+
+        return hasInstall;
     }
 
     // 获取当前日期(年月日时分秒)字符串(不带分隔符)
